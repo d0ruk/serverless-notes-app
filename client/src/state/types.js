629217ae01f892f7ba45200ff885630491067fe5
Doruk Kutlu <dorukk@gmail.com>
2018-01-16 00:49:55 +0300

    f: notes actions/reducer

--- client/src/state/types.js
+++ client/src/state/types.js
@@ -1,8 +1,17 @@
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
-export const EMAIL = "EMAIL";
-export const PASSWORD = "PASSWORD";
+export const SET_EMAIL = "SET_EMAIL";
+export const SET_PASSWORD = "SET_PASSWORD";
 export const SET_USER = "SET_USER";
 export const SIGNUP = "SIGNUP";
 export const RESEND = "RESEND";
 export const VERIFY = "VERIFY";
+
+export const SET_CONTENT = "SET_CONTENT";
+export const PUT_FILE = "PUT_FILE";
+export const CREATE_NOTE = "CREATE_NOTE";
+export const GET_FILE = "GET_FILE";
+export const GET_NOTES = "GET_NOTES";
+export const DELETE_NOTE = "DELETE_NOTE";
+export const GET_NOTE = "GET_NOTE";
+export const UPDATE_NOTE = "UPDATE_NOTE";
