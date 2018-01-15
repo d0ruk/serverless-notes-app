39e2f970c6118f7fb2a61fa0640c08a33675e40b
Doruk Kutlu <dorukk@gmail.com>
2018-01-16 01:04:57 +0300

    f: sync NavBar with location | dev actions - add notes & debug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-import { Layout } from "antd";
+import { Layout, notification } from "antd";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 import { object, func } from "prop-types";
 import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
+import range from "lodash/range";
+import randSentence from "random-sentence";
+import debug from "debug";
 
 import styles from "./App.css";
 import NavBar from "./components/NavBar";
@@ -15,19 +18,22 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import NewNote from "./pages/NewNote";
 
 import { logoutUser } from "./state/actions/auth-actions";
+import { createNote } from "./state/actions/notes-actions";
 
 @withRouter
 @connect(
   ({ auth }) => ({ cognitoUser: auth.cognitoUser }),
-  { logoutUser },
+  { logoutUser, createNote },
 )
 export default class App extends Component {
   static childContextTypes = { cognitoUser: object };
   static propTypes = {
     cognitoUser: object, // TODO: shapeOf
     logoutUser: func.isRequired,
+    createNote: func.isRequired,
   };
 
   getChildContext() {
@@ -36,7 +42,17 @@ export default class App extends Component {
     };
   }
 
+  componentWillMount() {
+    notification.config({
+      placement: "topRight",
+      top: 10,
+      duration: 3,
+    });
+  }
+
   render() {
+    const { cognitoUser } = this.props;
+
     return (
       <Layout className={styles.wrapper}>
         <Helmet
@@ -44,7 +60,11 @@ export default class App extends Component {
           titleTemplate="Scratch | %s"
         />
         <Layout.Sider collapsed>
-          <NavBar />
+          <NavBar
+            handleAdd={this.handleAdd}
+            handleDebug={this.handleDebug}
+            debugEnabled={Boolean(window.localStorage.getItem("debug"))}
+          />
         </Layout.Sider>
         <Layout>
           <Layout.Content className={styles.content}>
@@ -52,15 +72,17 @@ export default class App extends Component {
               <AuthRoute path="/" exact component={Home} />
               <UnAuthRoute path="/login" exact component={Login} />
               <UnAuthRoute path="/signup" exact component={Signup} />
+              <AuthRoute path="/add" exact component={NewNote} />
               <Route
                 path="/logout"
                 exact
                 render={() => {
-                  if (this.props.cognitoUser) {
+                  if (cognitoUser) {
                     this.props.logoutUser();
                   }
-
-                  return <Redirect to="/" />;
+                  // TODO: BUG: redirects to / while action in progress
+                  // thus GET_NOTES is dispatched without a user in state
+                  return <Redirect to="/login" />;
                 }}
               />
               <Route component={NotFound} />
@@ -70,5 +92,36 @@ export default class App extends Component {
         </Layout>
       </Layout>
     );
+  }
+
+  handleAdd = evt => {
+    if (!this.props.cognitoUser) return;
+    const { amount } = evt.target.dataset;
+
+    notification.info({
+      message: `Adding ${amount} random notes.`,
+      key: "add-notes",
+      duration: 0,
+    });
+
+    range(amount)
+      .reduce(acc => acc.then(() => {
+        const content = randSentence({ min: 4, max: 42 });
+        return this.props.createNote({ content });
+      }), Promise.resolve())
+      .catch(console.error) // eslint-disable-line
+      .finally(() => {
+        notification.close("add-notes");
+      });
+  }
+
+  handleDebug = evt => {
+    const { checked } = evt.target;
+
+    if (checked) {
+      debug.enable("*,-sockjs*");
+    } else {
+      debug.disable();
+    }
   }
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,47 +1,96 @@
 import React from "react";
-import { Menu, Icon } from "antd";
+import { Menu, Icon, Checkbox } from "antd";
 import { Link, withRouter } from "react-router-dom";
-import { object } from "prop-types";
+import { object, func } from "prop-types";
+import { location as locationProps } from "react-router-prop-types";
 
-function getRoutes(loggedIn) {
-  return [
-    {
-      href: "/",
-      iconType: "home",
-      name: "Home",
-    },
-  ].concat(loggedIn
-    ? [{
-      href: "/logout",
-      iconType: "logout",
-      name: "Logout",
-    }]
-    : [{
-      href: "/login",
-      iconType: "login",
-      name: "Login",
-    },
-    {
-      href: "/signup",
-      iconType: "user-add",
-      name: "Sign Up",
-    }]);
-}
+import Button from "./Button";
 
-const NavBar = (_, { cognitoUser }) => (
-  <Menu theme="dark">
-    {getRoutes(Boolean(cognitoUser)).map(({ name, iconType, href }) => (
-      <Menu.Item key={name}>
-        <Icon type={iconType} />
-        <Link to={href}>{name}</Link>
-      </Menu.Item>
-    ))}
-  </Menu>
-);
+const isProd = process.env.NODE_ENV === "production";
+const getRoutes = loggedIn => [{
+  href: "/",
+  iconType: "home",
+  name: "Home",
+}].concat(loggedIn
+  ? [{
+    href: "/add",
+    iconType: "plus-circle",
+    name: "Add Note",
+  }, {
+    href: "/logout",
+    iconType: "logout",
+    name: "Logout",
+  }]
+  : [{
+    href: "/login",
+    iconType: "login",
+    name: "Login",
+  }, {
+    href: "/signup",
+    iconType: "user-add",
+    name: "Sign Up",
+  }]);
+
+const NavBar = ({ location, ...rest }, { cognitoUser }) => {
+  const validRoutes = getRoutes(Boolean(cognitoUser));
+  // currentRoute might be undefined between redirect renders
+  const currentRoute = validRoutes.find(e => e.href === location.pathname);
+
+  return (
+    <Menu
+      theme="dark"
+      selectedKeys={currentRoute ? [currentRoute.href] : []}
+      style={{ tabIndex: -1 }}
+    >
+      {validRoutes.map(({ name, iconType, href }) => (
+        <Menu.Item key={href}>
+          <Icon type={iconType} />
+          <Link to={href}>{name}</Link>
+        </Menu.Item>
+      ))}
+      {!isProd && renderSettings({ ...rest })}
+    </Menu>
+  );
+};
+
+NavBar.propTypes = {
+  location: locationProps,
+  handleAdd: func.isRequired,
+};
 
 NavBar.contextTypes = {
   cognitoUser: object,
 };
 
-// TODO: either sync the router state with the menu state or use a different component
 export default withRouter(NavBar);
+
+
+/* eslint-disable */
+function renderSettings({ handleAdd, handleDebug, debugEnabled }) {
+  return (
+    <Menu.SubMenu
+      key="setting"
+      title={<Icon type="setting" />}
+    >
+      {[10, 25, 50].map(amount => (
+        <Menu.Item key={`add_${amount}`}>
+          <Button
+            onClick={handleAdd}
+            data-amount={amount}
+          >
+          Add {amount} notes
+          </Button>
+        </Menu.Item>
+      ))}
+      <Menu.Item>
+        <Checkbox
+          defaultChecked={debugEnabled}
+          onChange={handleDebug}
+          style={{ color: "lightgray" }}
+        >
+          debug
+        </Checkbox>
+      </Menu.Item>
+    </Menu.SubMenu>
+  );
+}
