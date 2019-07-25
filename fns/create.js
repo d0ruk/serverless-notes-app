8949610b277b5771739c513717217732b6aa5c65
Doruk Kutlu <me@doruk.me>
2019-07-25 19:04:40 +0300

    return statusCode 201 from createNote

--- fns/create.js
+++ fns/create.js
@@ -1,7 +1,7 @@
 import uuid from "uuid"
 import middy from "middy"
 import { cors, jsonBodyParser, httpHeaderNormalizer } from "middy/middlewares"
-import { success, failure, callDb } from "../util.js"
+import { buildResponse, failure, callDb } from "../util.js"
 import logger from "lambda-log"
 
 const { TableName } = process.env;
@@ -27,7 +27,7 @@ async function createNote(evt, ctx) {
   logger.info(`Creating note ${noteId} for ${userId}`);
   try {
     await callDb("put", params);
-    return success(params.Item);
+    return buildResponse(201, params.Item);
   } catch(err) {
     logger.error("Error @ db.put", { err, env: process.env });
     return failure({ error: err.message });
